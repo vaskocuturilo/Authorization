ed5377f4945f5fb2f7930af2be584f706a4b61f2
Vasko <vaskocuturilo@rambler.ru`>
2019-12-16 17:03:45 +0300

    Feat : BaseWeb :  Added methods beforeTest and afterTest. Feat : DriverHolder : Added getter and setter.
    Commit at 16/12/2019

--- src/main/java/ru/dostavista/Page.java
+++ src/main/java/ru/dostavista/Page.java
@@ -1,4 +1,50 @@
 package ru.dostavista;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import ru.dostavista.base.DriverHolder;
+
+/**
+ * The class Abstract Page.
+ */
 public abstract class Page {
+
+    /**
+     * Method navigateWebBrowser.
+     *
+     * @return the web driver . navigation
+     */
+    public WebDriver.Navigation navigateWebBrowser() {
+        return DriverHolder.getDriverThread().navigate();
+    }
+
+    /**
+     * Method getWindowTitle.
+     *
+     * @return the window title
+     */
+    public String getWindowTitle() {
+
+        return DriverHolder.getDriverThread().getTitle();
+    }
+
+
+    /**
+     * Method closeWebBrowser.
+     */
+    public static void closeWebBrowser() {
+        DriverHolder.getDriverThread().manage().deleteAllCookies();
+        DriverHolder.getDriverThread().close();
+    }
+
+    /**
+     * Find element web element.
+     *
+     * @param locator this is locator of element.
+     * @return the web element.
+     */
+    public WebElement findElement(final By locator) {
+        return DriverHolder.getDriverThread().findElement(locator);
+    }
 }
--- src/main/java/ru/dostavista/base/BaseWeb.java
+++ src/main/java/ru/dostavista/base/BaseWeb.java
@@ -1,4 +1,17 @@
 package ru.dostavista.base;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
 public class BaseWeb {
+
+    @BeforeTest
+    public void beforeTest() {
+
+    }
+
+    @AfterTest
+    public void afterTest() {
+
+    }
 }
