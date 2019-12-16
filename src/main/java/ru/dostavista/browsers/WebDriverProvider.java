d688b0f3d41be79cedf104a90bf4a372c3d9254c
Vasko <vaskocuturilo@rambler.ru`>
2019-12-16 17:07:29 +0300

    Feat : WebDriverProvider :  Added interface.
    Commit at 16/12/2019

---
+++ src/main/java/ru/dostavista/browsers/WebDriverProvider.java
@@ -0,0 +1,20 @@
+package ru.dostavista.browsers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+/**
+ * The interface Web driver provider.
+ */
+public interface WebDriverProvider {
+
+    /**
+     * Create new {@link WebDriver} instance.
+     *
+     * @param capabilities set of desired capabilities as suggested by Selenium framework;
+     *                     method implementation is recommended to pass this variable to {@link WebDriver},
+     *                     probably modifying it according to specific needs.
+     * @return new {@link WebDriver} instance.
+     */
+    WebDriver createDriver(DesiredCapabilities capabilities);
+}
