6ec02a6d507fd97e3d2c39a9f359ed0ab7463f44
Vasko <vaskocuturilo@rambler.ru`>
2019-12-17 12:47:08 +0300

    Feat : ProvideData : Added data provider methods invalidCredential and validCredential.
    Commit at 17/12/2019

---
+++ src/main/java/ru/dostavista/userdata/ProvideData.java
@@ -0,0 +1,36 @@
+package ru.dostavista.userdata;
+
+import org.testng.annotations.DataProvider;
+import ru.dostavista.model.User;
+
+
+/**
+ * The class Provide data.
+ */
+public class ProvideData {
+
+    /**
+     * Method Invalid data object [ ] [ ].
+     *
+     * @return the object [ ] [ ]
+     */
+    @DataProvider(name = "invalidCredential")
+    public Object[][] invalidCredential() {
+        return new Object[][]{
+                new Object[]{new User("test1", "test1")},
+                new Object[]{new User("test2", "test2")},
+                new Object[]{new User("test2", "test2")}};
+    }
+
+    /**
+     * Method Valid credential object [ ] [ ].
+     *
+     * @return the object [ ] [ ]
+     */
+    @DataProvider(name = "validCredential")
+    public Object[][] validCredential() {
+        return new Object[][]{
+                new Object[]{new User("admin", "admin")}};
+    }
+
+}
